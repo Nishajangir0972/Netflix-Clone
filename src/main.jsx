ccda762d842016816d0d9fa4c0690cd858e36f50
Nishajangir0972 <nishajangir9302@gmail.com>
2023-09-19 23:46:33 -0700

    first

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './form/form.jsx'
-import'./form/Netflix/App.css'
-import App from './form/Netflix/App'
+import'./Netflix/App.css'
+import App from './Netflix/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
